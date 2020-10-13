f7084dc202383d7bfa6767bbbfb80d480979b8e5
Piraldo <guilherme.msb99@gmail.com>
2020-10-13 17:04:37 -0300

    Transição de cenas completa

---
+++ Janela/Assets/Scripts/Menuprincipal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menuprincipal : MonoBehaviour
+{
+    public Animator transition;
+    public float TransitionTime = 1f;
+
+    public void PlayGame ()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+   public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("comeco");
+
+        yield return new WaitForSeconds(TransitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
+
