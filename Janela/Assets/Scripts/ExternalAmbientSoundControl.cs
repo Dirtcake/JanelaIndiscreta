c193187225743747d935166533a94f979e134b48
Gabriel Tonza <gtonza@yahoo.com.br>
2020-10-22 18:47:07 -0300

    shader e ambiencia externa
    setei o shader e deixei evento de som externo pronto pro day cycle.
    valores:
    0 = manhã, 1 = tarde e 3 = noite.

---
+++ Janela/Assets/Scripts/ExternalAmbientSoundControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalAmbientSoundControl : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string eAmbientEvent;
+    FMOD.Studio.EventInstance eAmbience;
+
+    public int Daytime;
+    public bool isPlaying;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eAmbience = FMODUnity.RuntimeManager.CreateInstance(eAmbientEvent);
+
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(eAmbience, GetComponent<Transform>(), GetComponent<Rigidbody>());
+
+        eAmbience.start();
+
+        if (isPlaying == true)
+        {
+            //Ambience();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Daytime >= 0 && Daytime <= 2)
+        {
+            if(Daytime == 0)
+            {
+                eAmbience.setParameterByName("Daytime", 0);
+            }
+
+            if (Daytime == 1)
+            {
+                eAmbience.setParameterByName("Daytime", 1);
+            }
+
+            if (Daytime == 2)
+            {
+                eAmbience.setParameterByName("Daytime", 2);
+            }
+        }
+    }
+
+    void Ambience()
+    {
+        if (isPlaying == true)
+        {
+            eAmbience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            isPlaying = false;
+        }
+        else if (isPlaying == false)
+        {
+            eAmbience.start();
+            isPlaying = true;
+        }
+    }
+}
