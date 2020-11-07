8c9fd2e91b0de6de8704609bf6c68907d02fece3
Piraldo <guilherme.msb99@gmail.com>
2020-11-07 19:36:40 -0300

    Pause script
    nao consigo testar, talvez eu precise reimportar alguma coisa

---
+++ Janela/Assets/MenuPausa.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    public static bool JogoPausado = false;
+
+    public GameObject menuPauseUI;
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (JogoPausado)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume ()
+    {
+        menuPauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        JogoPausado = false;
+    }
+
+    void Pause ()
+    {
+        menuPauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        JogoPausado = true;
+    }
+}
