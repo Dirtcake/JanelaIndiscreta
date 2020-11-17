ba2ad8c7631e54e10bdc6ca188ae86739ed809e6
ianmagdaleno <54992934+ianmagdaleno@users.noreply.github.com>
2020-11-17 19:30:52 -0300

    comeco aplicar relogio

---
+++ Janela/Assets/Relogio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Relogio : MonoBehaviour
+{
+    public GameObject relogio;
+    public void OnMouseEnter()
+    {
+        Debug.Log("ta em cima ");
+        relogio.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        Debug.Log("relogio");
+        relogio.SetActive(false);
+    }
+}
